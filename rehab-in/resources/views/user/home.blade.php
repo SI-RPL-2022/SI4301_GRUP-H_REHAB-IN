5f685baf90d35934ca85c4761bf819c3b795cb8d
nurul90042 <wahdanurul.nw@gmail.com>
2022-04-15 07:46:22 +0700

    Add: Content Homepage Pasien ref #5

--- rehab-in/resources/views/user/home.blade.php
+++ rehab-in/resources/views/user/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('main')
 
+{{-- NEWS --}}
   <link rel="stylesheet" type="text/css" href="{{ asset('assets/style/user/css/article.css') }}">
   <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script> 
 
@@ -76,79 +77,147 @@
   });
   </script>
   
-  
-  <div id="contact" class="contact-us section" style="margin-top: 50px;">
+{{-- NEWS --}}
+
+
+{{-- CURRENT SCHEDULE --}}
+<div id="contact" class="contact-us section" style="margin-top: 30px;">
+  <div class="row">
+    <div class="col-lg-12 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.25s">
+      <form id="contact" action="" method="post">
+          <div class="col-lg-12 schedule-card">
+            <div class="user-pict-schedule">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="user-profile-header">Konsultasi Hari Ini</h4>
+                        <div class="info-post row" style="padding-top: 0px;padding-bottom: 0px">
+                          <div class="col-lg-4">  
+                            <img src="{{asset('assets/style/images/doc1.png')}}" alt=""/>
+                            <h5 style="font-weight:bold; padding-top:10px;">dr. Muhammad Syahid</h5>
+                            <h6 style="color:#fff;">Spesialis Jantung</h6>
+                          </div>
+                          <div class="col-lg-8 schedule-time-frame">
+                            {{-- CONTENT --}}
+                            <div class="user-pict-schedule" style="background-color: transparent; border-radius:23px;">
+                              <div class="col-lg-12">
+                                  <div class="row">
+                                    <div class="col-lg-6" style="text-align: left;">
+                                      <div class="col-lg-12">
+                                        <div class="row">
+                                          <div class="col-lg-2">
+                                            <span class="fa fa-calendar fa-2x"></span>
+                                          </div>
+                                          <div class="col-lg-10">
+                                            <p class="schedule-content">Jumat, 25 Maret 2022</p>
+                                          </div>
+                                        </div>  
+                                      </div>
+                                      <div class="col-lg-12">
+                                        <div class="row">
+                                          <div class="col-lg-2">
+                                            <span class="fa fa-clock-o fa-2x"></span>
+                                          </div>
+                                          <div class="col-lg-10">
+                                            <p class="schedule-content">11.00 - 13.00 WIB</p>
+                                          </div>
+                                        </div>  
+                                      </div>
+                                    </div>
+                                    <div class="col-lg-6" style="text-align: left;">
+                                      <div class="col-lg-12">
+                                        <div class="row">
+                                          <div class="col-lg-2">
+                                            <span class="fa fa-comments-o fa-2x"></span>
+                                          </div>
+                                          <div class="col-lg-10">
+                                            <p class="schedule-content">Jumat, 25 Maret 2022</p>
+                                          </div>
+                                        </div>  
+                                      </div>
+                                    </div>
+                                  </div>
+                              </div>                              
+                          </div>
+                          {{-- CONTENT --}}
+                          </div>
+                        </div>
+
+                      </div>
+                        </div>
+                        
+                        </div>
+                </div>
+              </div>
+            </div>
+        
+      </form>
+  </div>
+</div>
+</div>
+{{-- CURRENT SCHEDULE --}}
+
+
+
+
+{{-- HEALTH NOTES --}}
+
+  <div id="contact content" class="p-4 p-md-5" style="">
     <div class="row">
       <div class="col-lg-12 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.25s">
         <form id="contact" action="" method="post">
-      
-            <div class="col-lg-12">
-              <div class="user-pict" style="padding: 20px 2px 20px 60px;">
+              <div class="user-pict-notes" style="padding: 20px 2px 20px 60px;">
                 <div class="row">
                   <div class="col-lg-12">
-                      <h4 class="user-profile-header">Artikel Kesehatan</h4>
-                          <div class="info-post row">
-                            
-
+                      <h4 class="user-profile-header" style="padding-right: 50px;">Catatan Kesehatan</h4>
+                          <div class="info-post row" style="padding-top: 0px;padding-bottom: 0px">
                               {{-- CONTENT --}}
-                              <div class="user-pict" style="background-color: #000; border-radius:23px; max-width:20%;margin-left:3%;">
-                                  <div class="col-lg-12">
-                                      <h4 class="user-profile-header">Artikel</h4>
-                                          <div class="info-post">
-                                            
-                                          </div>
+                              <div class="col-lg-3" style="border-radius:23px;">
+                                  <div class="">
+                                      <img src="{{asset('assets/style/user/images/jamsleep.png')}}" alt="">
+                                        <h6 style="color: #aaa; padding-top: 5px;">Waktu Tidur</h6>
+                                        <p style="font-weight: bold; font-size:15px; color:#000;">5 - 8 Jam</p>
                                   </div>                              
                               </div>
                               {{-- CONTENT --}}
-                            
-                           {{-- CONTENT --}}
-                           <div class="user-pict" style="background-color: #000; border-radius:23px; max-width:20%;margin-left:3%;">
-                            <div class="col-lg-12">
-                                <h4 class="user-profile-header">Artikel</h4>
-                                    <div class="info-post">
-                                      
-                                    </div>
+
+                              {{-- CONTENT --}}
+                              <div class="col-lg-3" style="background-color: transparent; border-radius:23px;">
+                                <div class="" style="padding: 0px 10px;">
+                                    <img src="{{asset('assets/style/user/images/sleepquality.png')}}" alt="">
+                                      <h6 style="color: #aaa; padding-top: 5px; min-width:50px;">Kualitas Tidur</h6>
+                                      <p style="font-weight: bold; font-size:15px; color:#000;">Normal</p>
+                                </div>                              
+                            </div>
+                            {{-- CONTENT --}}
+
+                            {{-- CONTENT --}}
+                            <div class="col-lg-3" style="background-color: transparent; border-radius:23px;">
+                              <div class="">
+                                  <img src="{{asset('assets/style/user/images/sport.png')}}" alt="">
+                                    <h6 style="color: #aaa; padding-top: 5px;">Olahraga</h6>
+                                    <p style="font-weight: bold; font-size:15px; color:#000;">Medium</p>
+                              </div>                              
+                          </div>
+                          {{-- CONTENT --}}
+
+                          {{-- CONTENT --}}
+                          <div class="col-lg-3" style="background-color: transparent; border-radius:23px;">
+                            <div class="">
+                                <img src="{{asset('assets/style/user/images/mood.png')}}" alt="">
+                                  <h6 style="color: #aaa; padding-top: 5px;">Mood</h6>
+                                  <p style="font-weight: bold; font-size:15px; color:#000;">Yeay!</p>
                             </div>                              
                         </div>
                         {{-- CONTENT --}}
-
-                        {{-- CONTENT --}}
-                        <div class="user-pict" style="background-color: #000; border-radius:23px; max-width:20%;margin-left:3%;">
-                          <div class="col-lg-12">
-                              <h4 class="user-profile-header">Artikel</h4>
-                                  <div class="info-post">
-                                    
-                                  </div>
-                          </div>                              
-                      </div>
-                      {{-- CONTENT --}}
-
-                       {{-- CONTENT --}}
-                       <div class="user-pict" style="background-color: #000; border-radius:23px; max-width:20%;margin-left:3%;">
-                        <div class="col-lg-12">
-                            <h4 class="user-profile-header">Artikel</h4>
-                                <div class="info-post">
-                                  
-                                </div>
-                        </div>                              
-                    </div>
-                    {{-- CONTENT --}}
-                          </div>
-
                         </div>
-                          </div>
-                          </div>
-                  </div>
                 </div>
               </div>
-          
+
         </form>
-    </div>
   </div>
 </div>
-
-
-
-
-
+</div>
+  
 @endsection
+
+{{-- HEALTH NOTES --}}
