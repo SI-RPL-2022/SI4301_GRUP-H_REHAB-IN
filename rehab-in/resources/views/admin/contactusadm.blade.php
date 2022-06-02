1048b09bc0469a6cad4f1a21c6537209b66c81c3
nurul90042 <wahdanurul.nw@gmail.com>
2022-06-02 09:42:12 +0700

    Add: View kontak kami ref #40

--- rehab-in/resources/views/admin/contactusadm.blade.php
+++ rehab-in/resources/views/admin/contactusadm.blade.php
@@ -1,3 +1,64 @@
 @extends('layouts.dashboard-admin')
 @section('main')
+
+{{-- TITLE --}}
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Kontak Kami</h1>
+    {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+        class="fas fa-plus fa-sm text-white-50"></i> Tambahkan Jadwal</a> --}}
+
+</div>
+{{-- TITLE --}}
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Kontak Kami</h6>
+            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i
+                class="fas fa-download fa-sm text-white-50"></i> Download CSV File</a> --}}
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nama</th> {{-- take from table pasien with ID Pasien --}}
+                            <th>Email</th>
+                            <th>Pesan</th>
+                            <th style="width:11%;">Aksi</th>
+                        </tr>
+                    </thead>
+                
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <th>Denis</th> {{-- take from table pasien with ID Pasien --}}
+                            <th>denis@mail</th>
+                            <th>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</th>
+                         
+                            <td>
+                                {{-- <a href="#" class="btn btn-info btn-icon-split">
+                                    <span class="icon text-white-50">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </span>
+                                </a> --}}
+                                <a href="#" class="btn btn-danger btn-icon-split">
+                                    <span class="icon text-white-50">
+                                        <i class="fas fa-trash"></i>
+                                    </span>
+                                </a>
+                            
+                            </td>
+                        </tr>
+                 
+                        
+                       
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
