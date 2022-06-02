576bd4fe59ed4a7acff23de70a6fdb7c34936b3f
nurul90042 <wahdanurul.nw@gmail.com>
2022-06-02 10:17:53 +0700

    Add: Riwayat view - admin area ref #32

--- rehab-in/resources/views/admin/riwayatadm.blade.php
+++ rehab-in/resources/views/admin/riwayatadm.blade.php
@@ -1,3 +1,65 @@
 @extends('layouts.dashboard-admin')
 @section('main')
+
+{{-- TITLE --}}
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Tabel Riwayat Pelayanan</h1>
+    {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+        class="fas fa-plus fa-sm text-white-50"></i> Tambahkan Jadwal</a> --}}
+
+</div>
+{{-- TITLE --}}
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Riwayat Pelayanan</h6>
+            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i
+                class="fas fa-download fa-sm text-white-50"></i> Download CSV File</a> --}}
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nama Pasien</th> {{-- take from table pasien with ID Pasien --}}
+                            <th>Jenis Layanan</th>
+                            <th>Pembayaran</th>
+                            <th style="width:11%;">Aksi</th>
+                        </tr>
+                    </thead>
+                
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <th>Farhan</th> {{-- take from table pasien with ID Pasien --}}
+                            <th>Reservasi Kamar</th>
+                            <th><a href="#" class="btn btn-success btn-icon-split">
+                                <span class="text">Lunas</span>
+                            </a></th>
+                            <td>
+                                {{-- <a href="#" class="btn btn-info btn-icon-split">
+                                    <span class="icon text-white-50">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </span>
+                                </a> --}}
+                                <a href="#" class="btn btn-danger btn-icon-split">
+                                    <span class="icon text-white-50">
+                                        <i class="fas fa-trash"></i>
+                                    </span>
+                                </a>
+                            
+                            </td>
+                        </tr>
+                 
+                        
+                       
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
